1ec16bc5c1909652b3d77ae752de3c6ad79b7689
mariams58 <mariams58@yahoo.com>
2024-10-14 15:04:38 +0100

    Python backend: task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Duck writing add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two numbers argumts of type float
+    Args:
+        a (float): a
+	b (float): b
+    Returns:
+        float: sum
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This is the readme for the task on Python Variable Annotation 
